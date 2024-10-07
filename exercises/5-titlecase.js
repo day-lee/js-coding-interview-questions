96e66a706f67afe65c0dc0ce3f4983cb855cac7c
Dayeon Lee <iamdayeonlee@gmail.com>
2024-10-07 12:46:09 +0100

    add 5

--- exercises/5-titlecase.js
+++ exercises/5-titlecase.js
@@ -7,6 +7,33 @@
 //   capitalize('what is titlecase?') --> 'What Is Titlecase?'
 //   capitalize('titles of books, movies, songs, plays and other works') --> 'Titles Of Books, Movies, Songs, Plays And Other Works'
 
-function capitalize(str) {}
+// option 1
+function capitalize(str) {
+  // turn the string into an array to use the for loop
+  const words = str.split(" ");
+  let result = [];
+  // push the transformed word into an array
+  for (let word of words) {
+    result.push(word[0].toUpperCase() + word.slice(1));
+  }
+  // turn it into a string
+  return result.join(" ");
+}
 
-console.log(capitalize('this is mukhtar from coding money'));
+console.log(capitalize("this is mukhtar from coding money"));
+console.log(capitalize("what is titlecase?"));
+console.log(
+  capitalize("titles of books, movies, songs, plays and other works")
+);
+
+// option 2. map()
+function capitalize2(str) {
+  const words = str.split(" ");
+  return words.map((word) => word[0].toUpperCase() + word.slice(1)).join(" ");
+}
+
+console.log(capitalize2("this is mukhtar from coding money"));
+console.log(capitalize2("what is titlecase?"));
+console.log(
+  capitalize2("titles of books, movies, songs, plays and other works")
+);
