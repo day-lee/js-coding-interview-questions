a1052ebd8a5d4d9d7788a348e7a5564571f1e80c
Dayeon Lee <iamdayeonlee@gmail.com>
2024-10-10 11:54:08 +0100

    add 8

--- exercises/8-steps-string-pattern.js
+++ exercises/8-steps-string-pattern.js
@@ -17,6 +17,47 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// similar to chessboard
+function steps(n) {
+  let pountSymbol = "#";
 
-steps(3)
+  for (let row = 1; row <= n; row++) {
+    let line = "";
+    for (let col = 0; col < n; col++) {
+      if (col < row) {
+        line += pountSymbol;
+      } else {
+        line += " ";
+      }
+    }
+    console.log(line);
+  }
+}
+
+steps(3);
+// '#\n##\n###\n'
+
+/*
+Chessboard
+Write a program that creates a string that represents an 8×8 grid, using
+newline characters to separate lines. At each position of the grid there
+is either a space or a "#" character. The characters should form a
+chessboard.
+Passing this string to console.log should show something like this:
+
+```jsx
+
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+```
+
+When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size,
+outputting a grid of the given width and height.
+
+*/
