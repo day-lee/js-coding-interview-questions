f06dc5e94203e19d2b7c61cacdceac2f5b1fa19a
Dayeon Lee <iamdayeonlee@gmail.com>
2024-10-11 11:22:07 +0100

    add chessboard

--- exercises/8-steps-string-pattern.js
+++ exercises/8-steps-string-pattern.js
@@ -60,3 +60,20 @@ When you have a program that generates this pattern, define a binding size = 8 a
 outputting a grid of the given width and height.
 
 */
+
+function chessboard(size) {
+  let line = "";
+  for (let row = 0; row < size; row++) {
+    for (let col = 0; col < size; col++) {
+      if ((row + col) % 2 === 0) {
+        line += " ";
+      } else {
+        line += "#";
+      }
+    }
+    line += "\n";
+  }
+  console.log(line);
+}
+
+chessboard(8);
