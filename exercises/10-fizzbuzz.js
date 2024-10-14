11e069afb7dd88d65782734046b69db29f9f5840
Dayeon Lee <iamdayeonlee@gmail.com>
2024-10-14 10:38:07 +0100

    add 10

--- exercises/10-fizzbuzz.js
+++ exercises/10-fizzbuzz.js
@@ -12,6 +12,33 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+//fizzBuzz(15);
 
-fizzBuzz(5)
+function fizzBuzz2(n) {
+  for (let i = 1; i <= n; i++) {
+    line = "";
+    if (i % 3 === 0) {
+      line += "fizz";
+    }
+    // seperate `if` clause!! not `else if`
+    if (i % 5 === 0) {
+      line += "buzz";
+    }
+    console.log(line ? line : i);
+  }
+}
+
+fizzBuzz2(15);
