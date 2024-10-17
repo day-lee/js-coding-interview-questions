c1f494ddf9543985f77b312eef35e5b941b71f6a
Dayeon Lee <iamdayeonlee@gmail.com>
2024-10-17 11:54:26 +0100

    add 1

--- exercises/1-reverse-int.js
+++ exercises/1-reverse-int.js
@@ -8,4 +8,37 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// option 1
+// convert a number to a string, then an array, reverse it
+// use Math.sign to put the original sign back
+function reverseInt(n) {
+  const str2 = String(Math.abs(n));
+  const reverse = parseInt(str2.split("").reverse().join(""));
+  return console.log(reverse * Math.sign(n));
+}
+
+reverseInt(15); //=== 51
+reverseInt(981); //=== 189
+reverseInt(500); //=== 5
+reverseInt(-15); //=== -51
+reverseInt(-90); //=== -9
+
+// option 2
+// convert a number to a string then an array. Create a new array
+// by filling in it reverse order, join it into a string, and convert it back to a number
+function reverseInt2(n) {
+  const arr = String(Math.abs(n)).split("");
+  let reversed = "";
+  for (let i = arr.length - 1; i >= 0; i--) {
+    reversed += arr[i];
+  }
+  return Number(reversed) * Math.sign(n);
+}
+
+console.assert(reverseInt2(15) === 51, "test1 fail");
+console.assert(reverseInt2(981) === 189, "test2 fail");
+console.assert(reverseInt2(500) === 5, "test3 fail");
+console.assert(reverseInt2(-15) === -51, "test4 fail");
+console.assert(reverseInt2(-90) === -9, "test5 fail");
+
+console.log("reverseInt2 test done");
--- exercises/1-reverse-string.js
+++ exercises/1-reverse-string.js
@@ -5,4 +5,44 @@
 //   reverse('hello') === 'olleh'
 //   reverse('CodingMoney') === 'yenoMgnidoC'
 
-function reverse(str) {}
+//option 1
+// utilise the built-in array function reverse(), split the string into an array, then join it back into a string
+function reverse(str) {
+  return str.split("").reverse().join("");
+}
+
+console.assert(reverse("hi") === "ih", "test1 fail");
+console.assert(reverse("hello") === "olleh", "test2 fail)");
+console.assert(reverse("CodingMoney") === "yenoMgnidoC", "test3 fail");
+console.log("reverse1 test done");
+
+//option2
+// create a new empty string, then loop through the parameter str in reverse order
+// adding each character to the new variable.
+function reverse2(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+console.assert(reverse2("hi") === "ih", "test1 fail");
+console.assert(reverse2("hello") === "olleh", "test2 fail)");
+console.assert(reverse2("CodingMoney") === "yenoMgnidoC", "test3 fail");
+console.log("reverse2 test done");
+
+// option3
+// Use a for...of loop to prepend each character to the 'reversed' string.
+function reverse3(str) {
+  let reversed = "";
+  for (let char of str) {
+    reversed = char + reversed;
+  }
+  return reversed;
+}
+
+console.assert(reverse3("hi") === "ih", "test1 fail");
+console.assert(reverse3("hello") === "olleh", "test2 fail)");
+console.assert(reverse3("CodingMoney") === "yenoMgnidoC", "test3 fail");
+console.log("reverse3 test done");
