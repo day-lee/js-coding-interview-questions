9c6034d68435c21bc48207ce882fbb3e59177cae
Dayeon Lee <iamdayeonlee@gmail.com>
2024-12-19 17:24:30 +0000

    add 22

--- completed_exercises/22-searachAndReplace.js
+++ completed_exercises/22-searachAndReplace.js
@@ -10,18 +10,20 @@ Third argument is what you will be replacing the second argument with (after).
 Note: Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word Book with the word dog, it should be replaced as Dog
 */
 
-// function myReplace(str, before, after) {
-//   let regex = /[A-Z]/;
+// 1. regex
+function myReplace(str, before, after) {
+  let regex = /[A-Z]/;
 
-//   if (regex.test(before)) {
-//     after = after[0].toUpperCase() + after.slice(1);
-//   } else {
-//     after = after[0].toLowerCase() + after.slice(1);
-//   }
+  if (regex.test(before)) {
+    after = after[0].toUpperCase() + after.slice(1);
+  } else {
+    after = after[0].toLowerCase() + after.slice(1);
+  }
 
-//   return console.log(str.replace(before, after));
-// }
+  return console.log(str.replace(before, after));
+}
 
+// 2. filter, map, ternary operator
 function myReplace(str, before, after) {
   // in te str, find before and replace it to after but with case sensitivity
   const arr = str.split(" ");
