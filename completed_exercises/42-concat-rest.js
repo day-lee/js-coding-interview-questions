7e85a3d186a3f16bb0a5a39b85afb85613de3fec
Dayeon Lee <iamdayeonlee@gmail.com>
2025-02-12 10:57:17 +0000

    add 42

--- completed_exercises/42-concat-rest.js
+++ completed_exercises/42-concat-rest.js
@@ -4,6 +4,7 @@ Create a function that concatenates n input arrays, where n is variable.
 Rest parameter is used because the number of parameters is not predetermined.
 */
 
+// option 1. recursive
 function concat(...args) {
   const arr = args;
   let result = [];
@@ -20,7 +21,24 @@ function concat(...args) {
   return result;
 }
 
+// option 2. rest parameter, spread operator
+// rest parameter to handle indefinite number of input
+function concat(...args) {
+  let result = [];
+
+  for (let array of args) {
+    if (Array.isArray(array)) {
+      // spread operator to concatenation
+      result.push(...array);
+    } else {
+      console.log("only arrays accepted,", array);
+    }
+  }
+  console.log(result);
+}
+
 concat([1, 2, 3], [4, 5], [6, 7]); // [1, 2, 3, 4, 5, 6, 7]
 concat([1], [2], [3], [4], [5], [6], [7]); // [1, 2, 3, 4, 5, 6, 7]
 concat([1, 2], [3, 4]); // [1, 2, 3, 4]
 concat([4, 4, 4, 4, 4]); // [4, 4, 4, 4, 4]
+concat(4, 4, 4, 4, 4);
